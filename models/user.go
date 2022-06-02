99705bbc3bde949141bf23d09873900664b06e44
majorchork <majorchork@gmail.com>
2022-06-02 02:29:39 +0100

    api v1

---
+++ models/user.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"strings"
+)
+
+type User struct {
+	ID           string `gorm:"primaryKey" json:"id"`
+	Username     string `gorm:"unique" json:"username"`
+	Name         string `json:"name"`
+	Age          uint   `json:"age"`
+	Email        string `json:"email"`
+	PasswordHash string `json:"-"`
+	Password     string `json:"password,omitempty" gorm:"-"`
+}
+
+func Capitalise(s string) string {
+	s = strings.ToLower(s)
+	var newString []byte
+	for i := 0; i < len(s); i++ {
+		if i == 0 {
+			newString = append(newString, s[i]-32)
+			continue
+		}
+		newString = append(newString, s[i])
+	}
+	return string(newString)
+}

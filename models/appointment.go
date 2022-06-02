22e18d8f3e1ae8575e95d9a98af9cc0648c4b63b
majorchork <majorchork@gmail.com>
2022-06-02 02:28:46 +0100

    api v1

---
+++ models/appointment.go
@@ -0,0 +1,16 @@
+package models
+
+type Appointment struct {
+	ID              string `json:"id" gorm:"primaryKey"`
+	Purpose         string `json:"purpose"`
+	PatientID       string `json:"patientID" gorm:"size:50"`
+	DoctorID        string `json:"doctorID" gorm:"size:50"`
+	Date            string `json:"date"`
+	AppointmentHour string `json:"appointmentHour"`
+	DoctorName      string `json:"doctorName"`
+	PatientName     string `json:"patientName"`
+}
+
+func (a Appointment) Add(val int) int {
+	return val + 1
+}
